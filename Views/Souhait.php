f486d029ba889a6adc240b5a6aa8f42739cdb154
Mimie1205 <anna.steinmetz@viacesi.fr>
2022-03-28 11:04:37 +0200

    html 1.1 css 1.0

--- Views/Souhait.php
+++ Views/Souhait.php
@@ -30,7 +30,7 @@ $results = $query->fetchALL(PDO::FETCH_OBJ);
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="/Public/css/souhait.css" />
-    <link rel="stylesheet" href="/Public/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="/Public/css/fontawesomecss/all.min.css">
     <link rel="stylesheet" href="/Public/css/style.css">
 
     <title>Souhait - PROJET WEB</title>
