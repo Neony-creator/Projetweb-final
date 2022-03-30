3de4f39d181b59a13653a373638180691b41ef53
Paulclaus67 <93578591+Paulclaus67@users.noreply.github.com>
2022-03-30 14:38:41 +0200

    Changement Acceuil.tpl to Accceuil.html parce que plus logique

--- Controllers/ControllerAccueil.php
+++ Controllers/ControllerAccueil.php
@@ -1,5 +1,5 @@
 <?php
 require("smarty/smarty.class.php"); // On inclut la classe Smarty
 $smarty = new Smarty();
-$smarty->display("C:/Users/pclau/IdeaProjects/Projetweb-final/Views/Accueil.tpl");
+$smarty->display("C:/Users/pclau/IdeaProjects/Projetweb-final/Views/Accueil.html");
 ?>
