b71e41bd0ae9093b7300fd32319a3295c5fb7f49
Mimie1205 <anna.steinmetz@viacesi.fr>
2022-03-30 16:14:03 +0200

    mobile

--- Views/Accueil.php
+++ Views/Accueil.php
@@ -8,7 +8,8 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="/Public/css/accueil.css" />
-    <link rel="Manifest" href="../Public/Json/Manifest.json">
+    <link rel="Manifest" href="../Public/json/Manifest.json">
+    <script src="../Public/js/script.js"></script>
     <title>PROJET WEB, groupe 4</title>
 </head>
 
